972e0ae9b7ffd59cd0993b7bb6abaff96f92fe0b
IvanKulaga <kulagaivanandreevich@gmail.com>
2024-02-23 22:52:40 +0600

    custom console not working despite being made as documentation says: https://camel.apache.org/manual/camel-console.html

---
+++ main/src/main/java/org/apache/camel/example/FooConsole.java
@@ -0,0 +1,36 @@
+package org.apache.camel.example;
+
+import org.apache.camel.spi.annotations.DevConsole;
+import org.apache.camel.support.console.AbstractDevConsole;
+import org.apache.camel.util.json.JsonObject;
+
+import java.util.Map;
+
+@DevConsole("foo")
+public class FooConsole extends AbstractDevConsole {
+
+    public FooConsole() {
+        super("acme", "foo", "Foolish", "A foolish console that outputs something");
+    }
+
+    @Override
+    protected String doCallText(Map<String, Object> options) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello from my custom console");
+
+        // more stuff here
+
+        return sb.toString();
+    }
+
+    @Override
+    protected JsonObject doCallJson(Map<String, Object> options) {
+        JsonObject root = new JsonObject();
+        root.put("message", "Hello from my custom console");
+
+        // more stuff here
+
+        return root;
+    }
+
+}
